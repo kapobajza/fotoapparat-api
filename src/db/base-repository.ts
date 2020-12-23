22deaf65d86fbd838d271052adf4f260152045be
Faruk <faruk.seremet@hotmail.com>
2020-12-23 11:33:30 +0100

    Refactored mysql store and base repository

--- src/db/base-repository.ts
+++ src/db/base-repository.ts
@@ -1,15 +1,59 @@
 import MySqlStore from './mysql';
-import IModel from './model-interface';
+import IModel, { ModelFieldType } from './model-interface';
 
 export interface QueryOptions {
   fields: string[];
   limit: number;
 }
 
+export type FieldMappingType = 'to_db_field' | 'to_object_field';
+
 export default class BaseRepository<T> {
-  private generateQuery(
+  private mapFields(
+    obj: { [key: string]: any },
+    fields: (string | ModelFieldType)[],
+    type: FieldMappingType = 'to_object_field'
+  ): { [key: string]: any } {
+    return fields.reduce((prevVal, currentVal) => {
+      let fieldName: string = '';
+      let fieldVal: any;
+      const fieldObj = currentVal as ModelFieldType;
+
+      if (typeof currentVal === 'string') {
+        fieldName = currentVal;
+        fieldVal = obj[currentVal];
+      } else if (fieldObj.db && fieldObj.to) {
+        switch (type) {
+          case 'to_object_field':
+            fieldName = fieldObj.to;
+            fieldVal = obj[fieldObj.db];
+            break;
+
+          case 'to_db_field':
+            fieldName = fieldObj.db;
+            fieldVal = obj[fieldObj.to];
+            break;
+
+          default:
+            break;
+        }
+      }
+
+      if (!fieldVal) {
+        return prevVal;
+      }
+
+      return {
+        ...prevVal,
+        [fieldName]: fieldVal,
+      };
+    }, {});
+  }
+
+  private async query(
     ModelType: { new (): IModel },
     extra?: string,
+    values?: any[],
     options?: QueryOptions,
     shouldReturnOne?: boolean
   ) {
@@ -24,20 +68,56 @@ export default class BaseRepository<T> {
       finalLimit = 1;
     }
 
-    return `SELECT ${fieldsStatement} FROM ${model.getName()} ${extra ?? ''} LIMIT ${finalLimit}`;
+    const results = await MySqlStore.executeQuery(
+      `SELECT ${fieldsStatement} FROM ${model.getName()} ${extra ?? ''} LIMIT ${finalLimit}`,
+      values
+    );
+
+    const finalResult = results.map((res: { [key: string]: any }) => {
+      const fields = model.getFields();
+      return this.mapFields(res, fields);
+    });
+
+    return shouldReturnOne ? finalResult[0] : finalResult;
   }
 
-  find(ModelType: { new (): IModel }, extra?: string, values?: any[], options?: QueryOptions) {
-    const query = this.generateQuery(ModelType, extra, options);
-    return MySqlStore.find<T>(ModelType, query, values);
+  find(
+    ModelType: { new (): IModel },
+    extra?: string,
+    values?: any[],
+    options?: QueryOptions
+  ): Promise<T[]> {
+    return this.query(ModelType, extra, values, options);
   }
 
-  findOne(ModelType: { new (): IModel }, extra?: string, values?: any[], options?: QueryOptions) {
-    const query = this.generateQuery(ModelType, extra, options, true);
-    return MySqlStore.findOne<T>(ModelType, query, values);
+  findOne(
+    ModelType: { new (): IModel },
+    extra?: string,
+    values?: any[],
+    options?: QueryOptions
+  ): Promise<T> {
+    return this.query(ModelType, extra, values, options, true);
   }
 
-  insert(ModelType: { new (): IModel }, data: { [key: string]: any }) {
-    return MySqlStore.insert<T>(ModelType, data);
+  async insert(ModelType: { new (): IModel }, data: { [key: string]: any }) {
+    const model = new ModelType();
+    const fields = model.getFields();
+    const mappedDbData = this.mapFields(data, fields, 'to_db_field');
+    delete mappedDbData.id;
+
+    const dbDataFields = Object.keys(mappedDbData).map((key) => key);
+    const dbDataValues = Object.values(mappedDbData);
+    const sqlQuery = `INSERT INTO \`${model.getName()}\` (${dbDataFields.join(
+      ', '
+    )}) VALUES (${dbDataValues.reduce((p, n) => (p += `${p ? ', ' : ''}'${n}'`), '')});`;
+
+    const results = await MySqlStore.executeQuery(sqlQuery);
+
+    const returnedData: any = {
+      ...data,
+      id: results?.insertId,
+    };
+
+    return returnedData;
   }
 }
--- src/db/mysql.ts
+++ src/db/mysql.ts
@@ -1,10 +1,7 @@
 import mysql, { Pool } from 'mysql';
 import moment from 'moment';
 
-import IModel, { ModelFieldType } from './model-interface';
 import Config from '../Config';
-
-type FieldMappingType = 'to_db_field' | 'to_object_field';
 
 class MySqlStore {
   connection: Pool;
@@ -19,48 +16,7 @@ class MySqlStore {
     });
   }
 
-  private mapFields(
-    obj: { [key: string]: any },
-    fields: (string | ModelFieldType)[],
-    type: FieldMappingType = 'to_object_field'
-  ): { [key: string]: any } {
-    return fields.reduce((prevVal, currentVal) => {
-      let fieldName: string = '';
-      let fieldVal: any;
-      const fieldObj = currentVal as ModelFieldType;
-
-      if (typeof currentVal === 'string') {
-        fieldName = currentVal;
-        fieldVal = obj[currentVal];
-      } else if (fieldObj.db && fieldObj.to) {
-        switch (type) {
-          case 'to_object_field':
-            fieldName = fieldObj.to;
-            fieldVal = obj[fieldObj.db];
-            break;
-
-          case 'to_db_field':
-            fieldName = fieldObj.db;
-            fieldVal = obj[fieldObj.to];
-            break;
-
-          default:
-            break;
-        }
-      }
-
-      if (!fieldVal) {
-        return prevVal;
-      }
-
-      return {
-        ...prevVal,
-        [fieldName]: fieldVal,
-      };
-    }, {});
-  }
-
-  private executeQuery(statements: string, values?: any[]): Promise<any> {
+  executeQuery(statements: string, values?: any[]): Promise<any> {
     const that = this;
 
     return new Promise((resolve, reject) => {
@@ -88,54 +44,6 @@ class MySqlStore {
         }
       });
     });
-  }
-
-  private async select<T>(
-    ModelType: { new (): IModel },
-    statements: string,
-    values?: any[]
-  ): Promise<T[]> {
-    const results = await this.executeQuery(statements, values);
-
-    const finalResult = results.map((res: { [key: string]: any }) => {
-      const model = new ModelType();
-      const fields = model.getFields();
-      return this.mapFields(res, fields);
-    });
-
-    return finalResult;
-  }
-
-  async findOne<T>(ModelType: { new (): IModel }, statements: string, values?: any[]): Promise<T> {
-    const result = await this.select<T>(ModelType, statements, values);
-    return result[0];
-  }
-
-  async find<T>(ModelType: { new (): IModel }, statements: string, values?: any[]): Promise<T[]> {
-    const result = await this.select<T>(ModelType, statements, values);
-    return result;
-  }
-
-  async insert<T>(ModelType: { new (): IModel }, data: { [key: string]: any }): Promise<T> {
-    const model = new ModelType();
-    const fields = model.getFields();
-    const mappedDbData = this.mapFields(data, fields, 'to_db_field');
-    delete mappedDbData.id;
-
-    const dbDataFields = Object.keys(mappedDbData).map((key) => key);
-    const dbDataValues = Object.values(mappedDbData);
-    const sqlQuery = `INSERT INTO \`${model.getName()}\` (${dbDataFields.join(
-      ', '
-    )}) VALUES (${dbDataValues.reduce((p, n) => (p += `${p ? ', ' : ''}'${n}'`), '')});`;
-
-    const results = await this.executeQuery(sqlQuery);
-
-    const returnedData: any = {
-      ...data,
-      id: results?.insertId,
-    };
-
-    return returnedData;
   }
 }
 
